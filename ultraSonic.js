848edf9cb624987a4c012db5bccedc78c717754b
kaliold <luis.aranda.lillo@gmail.com>
2023-09-07 23:11:07 +0200

    modified ultraSonic.js

--- ultraSonic.js
+++ ultraSonic.js
@@ -1,5 +1,15 @@
-new five.Proximity({
-    controller: "HCSR04",
-    pin: 12
-  });
+
+  var five = require("johnny-five");
+  var board = new five.Board();
   
+  board.on("ready", function() {
+    var proximity = new five.Proximity({
+      controller: "HCSR04",
+      pin: 12
+    });
+  
+    proximity.on("data", function() {
+      console.log("inches: ", this.inches);
+      console.log("cm: ", this.cm);
+    });
+  });  
